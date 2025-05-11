9d089baf0b2b1b599889d3be6f978ea741efaf5a
Dilani <dilanithathsarani2003@gmail.com>
2025-05-11 19:58:42 +0530

    json change

--- src/pages/Admin/products.jsx
+++ src/pages/Admin/products.jsx
@@ -5,3 +5,7 @@ export default function AdminProductsPage(){
         </div>
     )
 }
+
+
+//https://ffsrbwooenxdcqmujayu.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmc3Jid29vZW54ZGNxbXVqYXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1OTc2ODksImV4cCI6MjA2MjE3MzY4OX0.UDqMHqLcQWPMCtwKjd3HlNt1YrIH0urTJ2P8pGOaFaY
