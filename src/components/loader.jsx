653b8c609e2524ba74650e9c49e51dd022baaa7d
Dilani <dilanithathsarani2003@gmail.com>
2025-05-31 16:34:05 +0530

    loader colour updated

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,7 +1,7 @@
 export default function Loader() {
 	return (
 		<div className="w-full h-full  flex justify-center items-center">
-			<div className="w-[70px] h-[70px] border-[4px] border-transparent border-t-blue-900 rounded-full animate-spin"></div>
+			<div className="w-[70px] h-[70px] border-[4px] border-transparent border-t-[#86003C] rounded-full animate-spin"></div>
 		</div>
 	);
 }
